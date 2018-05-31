1cd4ab46639875bb5a9f84fc5c6f5fffa308565b
Elena-Iavorskaia <iavorska-o@ukr.net>
2018-05-31 16:41:26 +0300

    Изменила название кнопок

--- app/src/main/java/com/example/user/ourapp/Main2Activity_Drawer.java
+++ app/src/main/java/com/example/user/ourapp/Main2Activity_Drawer.java
@@ -88,7 +88,7 @@ public class Main2Activity_Drawer extends AppCompatActivity
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
+
 
         } else if (id == R.id.nav_send) {
 
