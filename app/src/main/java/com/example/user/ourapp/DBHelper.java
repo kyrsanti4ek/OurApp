00adb2b9d438c85823c1e48a752c8715da673858
Elena-Iavorskaia <iavorska-o@ukr.net>
2018-06-05 12:41:53 +0300

    Добавила класс, с помощью которого можно создать базу данных и добавить в нее данные

--- app/src/main/java/com/example/user/ourapp/DBHelper.java
+++ app/src/main/java/com/example/user/ourapp/DBHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
-// Класс для создания базы данных,
+    // Класс для создания базы данных,
 //    пример заполнения
     private static final int DATABASE_VERSION=1;
     private static final String DATABASE_NAME="contactDB";//название базы данных
@@ -18,7 +18,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
 
 
-    public DBHelper(Context context) {
+    private DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
@@ -28,7 +28,7 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override //метод для создания БД
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table " +TABLE_CONTACTS+ "(" + KEY_ID + " integer primary key autoincrement," + KEY_NAME+ " text,"+ KEY_MAIL+ " text" + ")");
-    //запрос создает таблицу
+        //запрос создает таблицу
 
         // метод для чтения и для записи
         // Можно добавить новую запись в таблицу с помощью SQL команды INSERT, а можно воспользоваться методом db.insert:
@@ -49,9 +49,9 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
 
-                db.execSQL("DROP TABLE IF EXISTS "+TABLE_CONTACTS);
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_CONTACTS);
 
-                onCreate(db);
+        onCreate(db);
 
     }
 
