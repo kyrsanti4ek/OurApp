aa06b160e34097abf485655bd68cab79cb85fd5c
Elena-Iavorskaia <iavorska-o@ukr.net>
2018-06-05 10:06:31 +0300

    Добавила класс, с помощью которого можно создать базу данных и добавить в нее данные

--- app/src/main/java/com/example/user/ourapp/MainActivity.java
+++ app/src/main/java/com/example/user/ourapp/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.user.ourapp;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.database.sqlite.SQLiteOpenHelper;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+
+
+
+
+
     Button btnActTwo;
+    DBHelper dbHelper;
 
     /** Called when the activity is first created. */
     @Override
@@ -17,12 +26,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       // db.execSQL("CREATE TABLE IF NOT EXISTS users (name TEXT, age INTEGER)");
+
         btnActTwo = (Button) findViewById(R.id.button);
         btnActTwo.setOnClickListener(this);
+
+        dbHelper = new DBHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
     }
 
     @Override
     public void onClick(View v) {
+
+
+
         switch (v.getId()) {
             case R.id.button:
 
