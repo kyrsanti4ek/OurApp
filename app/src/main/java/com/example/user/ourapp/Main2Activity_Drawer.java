df4741a03f4114b6586f277887514866d57eed4b
Yevhenii <35191297+kyrsanti4ek@users.noreply.github.com>
2018-06-07 15:29:39 +0300

    work weather in tab

--- app/src/main/java/com/example/user/ourapp/Main2Activity_Drawer.java
+++ app/src/main/java/com/example/user/ourapp/Main2Activity_Drawer.java
@@ -4,31 +4,33 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.user.ourapp.loginIn.LoginActivity;
 import com.example.user.ourapp.projIss.ProjectFragment;
-import com.example.user.ourapp.projIss.Project;
+import com.example.user.ourapp.weather.MainFragment;
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.squareup.picasso.Picasso;
-
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Main2Activity_Drawer extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
     private static final String TAG = "myLogs";
 
     private FirebaseAuth mAuth;
@@ -70,30 +72,32 @@ public class Main2Activity_Drawer extends AppCompatActivity
         };
 
 
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+/*        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);*/
 
         View headerView = navigationView.getHeaderView(0);
         name = (TextView) headerView.findViewById(R.id.nameAcc);
         imageView = (CircleImageView) headerView.findViewById(R.id.iconAcc);
 
         String personName = getIntent().getExtras().getString("name");
-
+        Log.e("onCreate", "yes");
         String personPhoto = getIntent().getExtras().getString("icon");
-        Log.d("PersonPhotoString",personPhoto);
+        Log.d("PersonPhotoString", personPhoto);
         name.setText(personName);
-            Picasso.get()
-                    .load(Uri.parse(personPhoto))
-                    .resize(200, 200)
-                    .centerCrop()
-                    .into(imageView);
+        Picasso.get()
+                .load(Uri.parse(personPhoto))
+                .resize(200, 200)
+                .centerCrop()
+                .into(imageView);
     }
 
     @Override
@@ -110,6 +114,7 @@ public class Main2Activity_Drawer extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main2_activity__drawer, menu);
+        Log.e("onCreateOptionsMenu", "yes");
         return true;
     }
 
@@ -119,13 +124,42 @@ public class Main2Activity_Drawer extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+        Log.e("onOptionsItemSelected", "yes");
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Log.e("action_settings", "yes");
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void displaySelectedScreen(int itemId) {
+
+        //creating fragment object
+        Fragment fragment = null;
+
+        //initializing the fragment object which is selected
+        switch (itemId) {
+            case R.id.nav_slideshow:
+                fragment = new MainFragment();
+                break;
+            case R.id.nav_camera:
+                showFragment(new FragmenPhoto());
+        }
+
+        //replacing the fragment
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+    }
+
+    public void showFragment(Fragment fragment) {
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.fragment_cont, fragment);
+            ft.commit();
+        }
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -137,14 +171,14 @@ public class Main2Activity_Drawer extends AppCompatActivity
         if (id == R.id.g_map) {
 
 
-
         } else if (id == R.id.nav_camera) {
 
-            getFragmentManager().beginTransaction().replace(R.id.fragment_cont, new FragmenPhoto()).commit();
+            //getFragmentManager().beginTransaction().replace(R.id.fragment_cont, new FragmenPhoto()).commit();
+            showFragment(new FragmenPhoto());
 
         } else if (id == R.id.nav_slideshow) {
 
-
+            showFragment(new MainFragment());
 
         } else if (id == R.id.nav_quotes) {
 
@@ -164,6 +198,7 @@ public class Main2Activity_Drawer extends AppCompatActivity
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+        //displaySelectedScreen(item.getItemId());
         return true;
     }
 
