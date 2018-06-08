063387af1ce1f55012f56749d0ab8dbb9c887c22
Elena-Iavorskaia <iavorska-o@ukr.net>
2018-06-08 16:34:25 +0300

    ! push уведомление из Firebase// добавила сообщение вверху

--- app/src/main/java/com/example/user/ourapp/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/user/ourapp/MyFirebaseMessagingService.java
@@ -1,6 +1,13 @@
 package com.example.user.ourapp;
 
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
@@ -29,20 +36,30 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
              Toast.makeText(getApplicationContext(),title + " , " + body,Toast.LENGTH_SHORT).show();
          }
      });
+
+        sendNotification(remoteMessage.getNotification().getBody());
     }
 
 
-    //public
-//
-//        Log.d(TAG, "Refreshed token: " + refreshedToken);
-//
-//        // If you want to send messages to this application instance or
-//        // manage this apps subscriptions on the server side, send the
-//        // Instance ID token to your app server.
-//        sendRegistrationToServer(refreshedToken);
-//
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_announcement_black_24dp)
+                .setLargeIcon(BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_announcement_black_24dp))
+                .setContentTitle(this.getString(R.string.app_name))
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
 
 
 }
