bcce407a05a9b786241832586041f73e1949c517
Elena-Iavorskaia <iavorska-o@ukr.net>
2018-06-11 16:10:05 +0300

    activity_login сделала Fullscreen, опустила кнопки

--- app/src/main/java/com/example/user/ourapp/loginIn/LoginActivity.java
+++ app/src/main/java/com/example/user/ourapp/loginIn/LoginActivity.java
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.example.user.ourapp.Main2Activity_Drawer;
@@ -73,7 +75,20 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
         setContentView(R.layout.activity_login);
+
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//
+//        setContentView(R.layout.activity_login);
 
 
         callbackManager = CallbackManager.Factory.create();
