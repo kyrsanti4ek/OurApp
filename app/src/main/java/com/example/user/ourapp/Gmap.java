4ec7f1e0681d0ae46aa981337aa49fcf012476e1
monagrig <monagrig@ukr.net>
2018-06-14 12:34:37 +0300

    рабочие карты, с указанием текущего местоположения

--- app/src/main/java/com/example/user/ourapp/Gmap.java
+++ app/src/main/java/com/example/user/ourapp/Gmap.java
@@ -1,19 +1,24 @@
 package com.example.user.ourapp;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -29,20 +34,17 @@ import com.google.android.gms.maps.model.MarkerOptions;
 // */
 
 
-public class Gmap extends Fragment implements /*GoogleMap.OnMyLocationButtonClickListener,
-        GoogleMap.OnMyLocationClickListener,*/ OnMapReadyCallback {
+public class Gmap extends Fragment implements GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMyLocationClickListener, OnMapReadyCallback {
 
     GoogleMap mGoogleMap;
     MapView mMapView;
     View mView;
 
-
-    public Gmap() {
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
 
@@ -67,18 +69,86 @@ public class Gmap extends Fragment implements /*GoogleMap.OnMyLocationButtonClic
 
 
     @Override
+    public void onMyLocationClick (@NonNull Location location){
+        Toast.makeText(getContext(), "Current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick () {
+        Toast.makeText(getContext(), "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+
+
+
+
+    private static final CharSequence[] MAP_TYPE_ITEMS =
+            {"Satellite", "Terrain", "Hybrid", "Normal"};
+
+    private void showMapTypeSelectorDialog() {
+        // Prepare the dialog by setting up a Builder.
+        final String fDialogTitle = "Select Map Type";
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle(fDialogTitle);
+
+        // Find the current map type to pre-check the item representing the current state.
+        int checkItem = mGoogleMap.getMapType() - 1;
+
+        // Add an OnClickListener to the dialog, so that the selection will be handled.
+        builder.setSingleChoiceItems(
+                MAP_TYPE_ITEMS,
+                checkItem,
+                new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int item) {
+                        // Locally create a finalised object.
+
+                        // Perform an action depending on which item was selected.
+                        switch (item) {
+                            case 1:
+                                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                                break;
+                            case 2:
+                                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                                break;
+                            case 3:
+                                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                                break;
+                            default:
+                                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                        }
+                        dialog.dismiss();
+                    }
+                }
+        );
+
+        // Build the dialog and show it.
+        AlertDialog fMapTypeDialog = builder.create();
+        fMapTypeDialog.setCanceledOnTouchOutside(true);
+        fMapTypeDialog.show();
+    }
+
+
+
+
+
+    @Override
     public void onMapReady(GoogleMap googleMap) {
 
-        MapsInitializer.initialize(getContext());
+        //MapsInitializer.initialize(getContext());
 
         mGoogleMap = googleMap;
-//        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(),
-//                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            return;}
-//
-//        mGoogleMap.setMyLocationEnabled(true);
-//        mGoogleMap.setOnMyLocationButtonClickListener(this);
-//        mGoogleMap.setOnMyLocationClickListener(this);
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        mGoogleMap.setMyLocationEnabled(true);
+        mGoogleMap.setOnMyLocationButtonClickListener(this);
+        mGoogleMap.setOnMyLocationClickListener(this);
 
 
         try {
@@ -111,32 +181,12 @@ public class Gmap extends Fragment implements /*GoogleMap.OnMyLocationButtonClic
 //        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
 //        googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
 //        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+   //     googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("My current location"));
-        CameraPosition cur = CameraPosition.builder().target(new LatLng(0, 0)).zoom(2).build();
-        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cur));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(50.45, 30.5)).title("My current location"));
+        //CameraPosition cur = CameraPosition.builder().target(new LatLng(0, 0)).zoom(2).build();
+        //googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cur));
     }
-
-//    @Override
-//    public void onMyLocationClick(@NonNull Location location) {
-//        Toast.makeText(getContext(), "Current location:\n" + location, Toast.LENGTH_LONG).show();
-//    }
-//
-//    @Override
-//    public boolean onMyLocationButtonClick() {
-//        Toast.makeText(getContext(), "MyLocation button clicked", Toast.LENGTH_SHORT).show();
-//        // Return false so that we don't consume the event and the default behavior still occurs
-//        // (the camera animates to the user's current position).
-//        return false;
-//    }
 }
-
-        //Marker in Kyiv
-//        googleMap.addMarker(new MarkerOptions().position(new LatLng(50.45, 30.50)).title("Kyiv"));
-//        CameraPosition kyiv = CameraPosition.builder().target(new LatLng(50.45, 30.50)).zoom(10).build();
-//        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(kyiv));
-
-
 
 
